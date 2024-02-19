b5b3f2f32727590d9e2ff82c72e06532d463fd58
Iru <bubi@MacBook-Pro-de-Iru-6.local>
2024-02-19 23:29:36 +0000

    add mounted, computed and watchers to handle window resizing and camera update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,41 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { useWindowSize } from '@vueuse/core';
 
+let renderer: WebGLRenderer;
+let camera: PerspectiveCamera;
 const experience = ref<HTMLCanvasElement | null>(null);
 const scene = new Scene();
 
-const camera = new PerspectiveCamera(75, window.innerWidth / innerHeight, 0.1, 1000);
+const { width, height } = useWindowSize();
+
+const aspectRatio = computed(() => width.value / height.value)
+
+function updateRenderer() {
+    renderer.setSize(width.value, height.value);
+    renderer.setPixelRatio(window.devicePixelRatio);
+}
+
+function updateCamera() {
+    camera.aspect = aspectRatio.value;
+    camera.updateProjectionMatrix;
+}
+
+watch(aspectRatio, updateRenderer);
+watch(aspectRatio, updateCamera);
+
+camera = new PerspectiveCamera(75, aspectRatio.value, 0.1, 1000);
+camera.position.z = 5;
 scene.add(camera);
 
 onMounted(() => {
-    const renderer = new WebGLRenderer({
+    renderer = new WebGLRenderer({
         canvas: experience.value as unknown as HTMLCanvasElement,
         antialias: true
     });
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(width.value, height.value);
     renderer.render(scene, camera);
 });
 
