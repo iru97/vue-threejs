b9852b1350e0ac07d6e1734e1e1d2f8721212f9f
Iru <bubi@MacBook-Pro-de-Iru-6.local>
2024-02-19 23:06:03 +0000

    add base scene, camera and render

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+
+const experience = ref<HTMLCanvasElement | null>(null);
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(75, window.innerWidth / innerHeight, 0.1, 1000);
+scene.add(camera);
+
+onMounted(() => {
+    const renderer = new WebGLRenderer({
+        canvas: experience.value as unknown as HTMLCanvasElement,
+        antialias: true
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+});
+
 
 </script>
 <template>
-  
+  <canvas ref="experience" />
 </template>
