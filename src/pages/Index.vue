d1ef353e0940e2d48c29cc9391a79d6504610eaf
Iru <bubi@MacBook-Pro-de-Iru-6.local>
2024-02-20 20:53:12 +0000

    add stl loader, snorlax model.stl and h1 with button to load the models

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { computed, onMounted, ref, watch } from 'vue';
-import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { AmbientLight, Color, Mesh, MeshPhysicalMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
 import { useWindowSize } from '@vueuse/core';
 
 let renderer: WebGLRenderer;
 let camera: PerspectiveCamera;
+let controls: OrbitControls;
+let loadedMesh: Mesh;
 const experience = ref<HTMLCanvasElement | null>(null);
 const scene = new Scene();
+const loader = new STLLoader();
+const light = new AmbientLight(0x404040);
 
 const { width, height } = useWindowSize();
 
@@ -22,14 +28,58 @@ function updateCamera() {
     camera.updateProjectionMatrix;
 }
 
+
+function loadSTLModel() {
+    loader.load(
+        '/assets/models/snorlax.stl',
+        (geometry) => {
+/*             const boxGeometry = new BoxGeometry(
+                1, 1, 1
+            ); */
+
+            const material = new MeshPhysicalMaterial({
+                color: 0xb2ffc8,
+                metalness: 0.25,
+                roughness: 0.1,
+                opacity: 1.0,
+                transparent: true,
+                transmission: 0.99,
+                clearcoat: 1.0,
+                clearcoatRoughness: 0.25
+            });
+            if (loadedMesh) scene.remove(loadedMesh);
+            /* const mesh = new Mesh(boxGeometry, new MeshBasicMaterial({color: 0x09FF95})); */
+            loadedMesh = new Mesh(geometry, material);
+            /* const mesh = new Mesh(geometry, new MeshLambertMaterial({color: 0x0ff00})); */
+            loadedMesh.scale.set(0.1, 0.1, 0.1);
+            loadedMesh.position.set(-10, -2, 0);
+            loadedMesh.rotation.x = -Math.PI / 2;
+
+            scene.add(loadedMesh);
+
+            loop();
+
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+}
+
 watch(aspectRatio, updateRenderer);
 watch(aspectRatio, updateCamera);
 
 camera = new PerspectiveCamera(75, aspectRatio.value, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 10;
 scene.add(camera);
-
+light.position.set(20, 20, 20)
+scene.add(light);
+scene.background = new Color(0x4c61d4);
 const loop = () => {
+    controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(loop);
 }
@@ -39,6 +89,8 @@ onMounted(() => {
         canvas: experience.value as unknown as HTMLCanvasElement,
         antialias: true
     });
+    controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true;
 
     updateRenderer();
     updateCamera();
@@ -49,5 +101,10 @@ onMounted(() => {
 
 </script>
 <template>
+    <div class="w-screen absolute text-white text-center top-2/4" style="transform: translateY(-50%);">
+		<h1 class="font-mono font-bold text-3xl tracking-wide">Iru Hernandez</h1>
+		<p class="font-exo font-bold text-6xl">Vue 3 + ThreeJS Visualizer</p>
+        <button class="mt-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center font-mono font-bold text-xl tracking-wide" @click="loadSTLModel">Load STL model</button>
+	</div>
   <canvas ref="experience" />
 </template>
